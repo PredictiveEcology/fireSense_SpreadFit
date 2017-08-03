f8ebea0eb9de57d9dd2c1c966392b39a1609d8d7
JM <jean.d.marchal@gmail.com>
2017-08-03 16:28:15 -0400

    Batch updates 2nd pass

--- fireSense_SpreadFit.R
+++ fireSense_SpreadFit.R
@@ -17,41 +17,47 @@ defineModule(sim, list(
   timeunit = NA_character_, # e.g., "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "fireSense_SpreadFit.Rmd"),
-  reqdPkgs = list("data.table", "DEoptim", "kSamples", "magrittr", "parallel", "raster"),
+  reqdPkgs = list("DEoptim", "kSamples", "magrittr", "parallel", "raster"),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
     defineParameter(name = "formula", class = "formula", default = NULL,
-                    desc = 'an object of class formula: a symbolic description 
-                            of the model to be fitted. Only the RHS needs to be 
-                            provided.'),
-    defineParameter(name = "lower", class = "numeric", default = NULL, desc = "see `?DEoptim`."),
-    defineParameter(name = "upper", class = "numeric", default = NULL, desc = "see `?DEoptim`."),
-    defineParameter(name = "data", class = "character", default = "dataFireSense_SpreadFit",
+                    desc = 'a formula describing the model to be fitted. Only 
+                            the RHS needs to be provided.'),
+    defineParameter(name = "data", class = "character", 
+                    default = "dataFireSense_SpreadFit",
                     desc = "a character vector indicating the names of objects 
                             in the `simList` environment in which to look for
-                            variables in the model. `data` objects can be
-                            RasterLayers or RasterStacks (for time series), or
-                            named list of either several RasterLayers or several
-                            RasterStacks, but RasterLayers and RasterStack can not
-                            be mixed together. If omitted, or if variables are
-                            not found in `data` objects, variables are searched
-                            in the `simList` environment."),
-    defineParameter(name = "fires", class = "SpatialPointsDataFrame", default = "fires",
-                    desc = "a character vector indicating the name of a 
-                            SpatialPointsDataFrame describing fires starting
-                            dates and locations, and final sizes. The 
-                            SpatialPointsDataFrame must have a column called
-                            'date' and one called 'size'."),
-    defineParameter(name = "trace", class = "numeric", default = 0,
-                    desc = "non-negative `integer`. If > 0, tracing information
-                            on the progress of the optimization are printed
-                            every `trace` iteration. Default is 0, which turns off
-                            tracing."),
-    defineParameter(name = "nCores", class = "integer", default = 0, 
-                    desc = "non-negative `integer`. Defines the number of 
+                            variables present in the model formula. `data`
+                            objects can be RasterLayers or RasterStacks (for
+                            time series). If variables are not found in `data`
+                            objects, they are searched in the `simList`
+                            environment."),
+    defineParameter(name = "fires", class = "character", 
+                    default = "fires",
+                    desc = "an object of class SpatialPointsDataFrame describing
+                            fires starting locations, final sizes ('size'
+                            column), and possibly the starting dates ('date'
+                            column) if fires are to be spread at different time
+                            intervals. If the 'date' column is not present, all
+                            fires are assumed to have started at the same time
+                            interval."),
+    defineParameter(name = "lower", class = "numeric", default = NULL,
+                    desc = "see `?DEoptim`."),
+    defineParameter(name = "upper", class = "numeric", default = NULL,
+                    desc = "see `?DEoptim`."),
+    defineParameter(name = "itermax", class = "integer", default = 500,
+                    desc = "integer defining the maximum number of iterations 
+                            allowed (DEoptim optimizer). Default is 500."),
+    defineParameter(name = "nCores", class = "integer", default = 1, 
+                    desc = "non-negative integer. Defines the number of 
                             logical cores to be used for parallel computation.
-                            The default value is 0, which disables parallel
+                            The default value is 1, which disables parallel
                             computing."),
+    defineParameter(name = "trace", class = "numeric", default = 0,
+                    desc = "non-negative integer. If > 0, tracing information on
+                            the progress of the optimization are printed every
+                            `trace` iteration. Default is 0, which turns off
+                            tracing."),    
     defineParameter(name = "initialRunTime", class = "numeric", default = start(sim),
                     desc = "when to start this module? By default, the start 
                             time of the simulation."),
@@ -65,20 +71,23 @@ defineModule(sim, list(
       objectClass = "SpatialPointsDataFrame",
       sourceURL = NA_character_,
       desc = "An object of class SpatialPointsDataFrame describing fires
-              starting dates and locations, and final sizes. It must have a 
-              'date' column and a 'size' column."
+              starting locations, final sizes ('size' column), and possibly the
+              starting dates ('date' column) if fires are to be spread at
+              different time intervals. If the 'date' column is not present, all
+              fires are assumed to have started at the same time interval."
     ),
     expectsInput(
       objectName = "dataFireSense_SpreadFit",
-      objectClass = "RasterLayer, RasterStack, list",
+      objectClass = "RasterLayer, RasterStack",
       sourceURL = NA_character_,
-      desc = "An object of class RasterLayer or RasterStack or named lists of 
-              RasterLayers or RasterStacks, in which to look for variables with
-              which to predict. RasterLayers and RasterStacks can not be mixed
-              together. RasterStacks can be used in cases where fires have
-              started at different years and should not be spread in the same
-              year of simulation, but are still used to describe a single fire
-              size distribution."
+      desc = "One or more objects of class 'RasterLayer' or 'RasterStack', in
+              which to look for variables present in the model formula.
+              RasterStacks can be used in cases where fires have started at
+              different times and should not be spread at the same time 
+              interval, but are still used to describe the same fire size
+              distribution. In this case, the number of layers in the
+              RasterStack should equal the number of distinct dates in column
+              'date'."
     )
   ),
   outputObjects = createsOutput(
@@ -129,7 +138,7 @@ fireSense_SpreadFitInit <- function(sim) {
   
   # Checking parameters
   stopifnot(P(sim)$trace >= 0)
-  stopifnot(P(sim)$nCores >= 0)
+  stopifnot(P(sim)$nCores >= 1)
   
   sim <- scheduleEvent(sim, eventTime = P(sim)$initialRunTime, current(sim)$moduleName, "run")
   sim
@@ -155,46 +164,104 @@ fireSense_SpreadFitRun <- function(sim) {
   envData <- new.env(parent = envir(sim))
   on.exit(rm(envData))
 
-  # Load data in the container
+  # Load inputs in the data container
   list2env(as.list(envir(sim)), envir = envData)
   
-  lapply(P(sim)$data, function(x, envData) {
-    
-    if (!is.null(sim[[x]])) {
-      
-      if (is.list(sim[[x]]) && !is.null(names(sim[[x]]))) {
-        
-        list2env(sim[[x]], envir = envData)
-        
-      } else stop(paste0(moduleName, "> '", x, "' is not a named list."))
-      
-    }
-    
-  }, envData = envData)
-
-  ## Mapping required object "fires" to one in the simList environment
+  if (!exists(P(sim)$fires, envData, inherits = FALSE))
+    stop(paste0(moduleName, "> '", P(sim)$fires, "' not found in the simList environment."))
+  
+  ## Map the "fires" parameter of this module to the "fires" object in the simList environment
   envData[["fires"]] <- envData[[P(sim)$fires]]
-
+  
+  if (!is(envData[["fires"]], "SpatialPointsDataFrame"))
+    stop(paste0(moduleName, "> '", P(sim)$fires, "' is not a SpatialPointsDataFrame."))
+  
+  if (is.null(envData[["fires"]][["size"]]))
+    stop(paste0(moduleName, "> The SpatialPointsDataFrame '", P(sim)$fires,"' must have a 'size' column."))
+  
+  sizes <- envData[["fires"]][["size"]]
+  
   if (is.empty.model(P(sim)$formula))
     stop(paste0(moduleName, "> The formula describes an empty model."))
 
   terms <- P(sim)$formula %>% terms.formula %>% delete.response ## If the formula has a LHS remove it
   allxy <- all.vars(terms)
   
-  if (all(unlist(lapply(allxy, function(x) is(envData[[x]], "RasterStack"))))) {
+  if (is.null(envData[["fires"]][["date"]])) { # All fires started during the same time interval
 
+    for(x in P(sim)$data) {
+      
+      if (!is.null(sim[[x]])) {
+        
+        if (is(sim[[x]], "RasterStack")) {
+          
+          list2env(setNames(unstack(sim[[x]]), names(sim[[x]])), envir = envData)
+          
+        } else if (is(sim[[x]], "RasterLayer")) {
+          
+          # Do nothing
+          
+        } else stop(paste0(moduleName, "> '", x, "' is not a RasterLayer or a RasterStack."))
+        
+      }
+      
+    }
+    
+    missing <- !allxy %in% ls(envData, all.names = TRUE)
+    
+    if (s <- sum(missing))
+      stop(paste0(moduleName, "> '", allxy[missing][1L], "'",
+                  if (s > 1) paste0(" (and ", s-1L, " other", if (s>2) "s", ")"),
+                  " not found in data objects nor in the simList environment."))
+    
+    
+    rasters <- mget(allxy, envir = envData, inherits = FALSE) %>% stack
+    
+    ## Get the corresponding loci from the raster sim$landscape for the fire locations
+    loci <- raster::extract(rasters, envData[["fires"]], cellnumbers = TRUE, df = TRUE)[["cells"]]
+    
+    if (anyDuplicated(loci)) stop(paste0(moduleName, "> No more than one fire can start in a given pixel."))
+    
+    objfun <- function(par, rasters, formula, loci, sizes, fireSense_SpreadFitRaster) {
+      
+      r <- predict(rasters, model = formula, fun = fireSense_SpreadFitRaster, na.rm = TRUE, par = par[5:length(par)]) %>%
+        calc(function(x) par[3L] + par[1L] / (1 + x^(-par[2L])) ^ par[4L]) ## 5-parameters logistic
+      
+      ## 10 replicates to better estimate the median
+      (lapply(1:10, function(i) tabulate(SpaDES::spread(r, loci = loci, spreadProb = r, returnIndices = TRUE)[["id"]])) %>%
+          do.call("rbind", .) %>%
+          apply(2L, median) %>%
+          list(sizes) %>%
+          ad.test %>%
+          `[[` ("ad")
+      )[1L, 1L]
+    }  
+    
+  } else { # Fires started at different time intervals
+    
+    missing <- !allxy %in% ls(envData, all.names = TRUE)
+    
+    if (any(missing))
+      stop(paste0(moduleName, "> '", paste(allxy[missing], collapse = "', '"), "' not found in data objects nor in the simList environment."))
+    
+    badClass <- !unlist(lapply(allxy, function(x) is(sim[[x]], "RasterLayer") || is(sim[[x]], "RasterStack")))
+    
+    if (any(badClass))
+      stop(paste0(moduleName, "> '", paste(allxy[badClass], collapse = "', '"), "' does not match a RasterLayer or a RasterStack."))
+    
+    
     rasters <- mget(allxy, envir = envData, inherits = FALSE) %>%
       lapply(unstack) %>%
       c(list(FUN = function(...) stack(list(...)), SIMPLIFY = FALSE)) %>%
       do.call("mapply", args = .)
-
+    
     ## Get the corresponding loci from the raster sim$landscape for the fire locations
     loci <- raster::extract(rasters[[1L]], envData[["fires"]], cellnumbers = TRUE, df = TRUE)[["cells"]]
-
+    
     loci %<>% split(envData[["fires"]][["date"]]) %>% lapply(na.omit)
-    lapply(loci, function(x) if (anyDuplicated(x)) stop(paste0(moduleName, "> No more than one fire can start in a given pixel.")) )
+    lapply(loci, function(x) if (anyDuplicated(x)) stop(paste0(moduleName, "> No more than one fire can start in a given pixel during the same time interval.")))
     sizes <- envData[["fires"]][["size"]]
-
+    
     objfun <- function(par, rasters, formula, loci, sizes, fireSense_SpreadFitRaster) {
       
       (rasters %>%
@@ -211,67 +278,30 @@ fireSense_SpreadFitRun <- function(sim) {
          unlist %>% list(sizes) %>% ad.test %>% `[[` ("ad"))[1L, 1L]
       
     }
-    
-  } else if (all(unlist(lapply(allxy, function(x) is(envData[[x]], "RasterLayer"))))) {
-    
-    rasters <- mget(allxy, envir = envData, inherits = FALSE) %>% stack
-    
-    ## Get the corresponding loci from the raster sim$landscape for the fire locations
-    loci <- raster::extract(rasters, envData[["fires"]], cellnumbers = TRUE, df = TRUE)[["cells"]]
-    
-    if (anyDuplicated(loci)) stop(paste0(moduleName, "> No more than one fire can start in a given pixel."))
-    
-    sizes <- envData[["fires"]][["size"]]
-    
-    objfun <- function(par, rasters, formula, loci, sizes, fireSense_SpreadFitRaster) {
-      
-      r <- predict(rasters, model = formula, fun = fireSense_SpreadFitRaster, na.rm = TRUE, par = par[5:length(par)]) %>%
-        calc(function(x) par[3L] + par[1L] / (1 + x^(-par[2L]))) ## 5-parameters logistic
-      
-      ## 10 replicates to better estimate the median
-      (lapply(1:10, function(i) tabulate(SpaDES::spread(r, loci = loci, spreadProb = r, returnIndices = TRUE)[["id"]])) %>%
-          do.call("rbind", .) %>%
-          apply(2L, median) %>%
-          list(sizes) %>%
-          ad.test %>%
-          `[[` ("ad"))[1L, 1L]
-      
-    }
-      
-  } else {
-    
-    exist <- allxy %in% ls(envData)
-    class <- unlist(lapply(allxy, function(x) is(envData[[x]], "RasterLayer") || is(envData[[x]], "RasterStack")))
-    
-    if (any(!exist)) {
-      stop(paste0(moduleName, "> Variable '", allxy[which(!exist)[1L]], "' not found."))
-    } else if (any(class)) {
-      stop(paste0(moduleName, "> Data objects are not of the same class (e.g. data.frames)."))
-    } else {
-      stop(paste0(moduleName, "> Variable '", allxy[which(!class)[1L]], "' does not match a RasterLayer or a RasterStack."))
-    }
+        
   }
-  
-  
-  control <- list(itermax = 2000, trace = P(sim)$trace)
-  
-  if (P(sim)$parallel) {
     
-    packages <- c("data.table", "kSamples", "magrittr", "raster")
+  control <- list(itermax = P(sim)$itermax, trace = P(sim)$trace)
+  
+  if (P(sim)$nCores > 1) {
     
-    cl <- parallel::makeCluster(P(sim)$parallel)
-    clusterEvalQ(cl, for (i in packages) library(i, character.only = TRUE))
+    cl <- parallel::makePSOCKcluster(names = P(sim)$nCores)
+    on.exit(stopCluster(cl))
+    clusterEvalQ(cl, for (i in c("kSamples", "magrittr", "raster")) library(i, character.only = TRUE))
     control$cluster <- cl
     
   }
   
-  val <- DEoptim(objfun, lower = P(sim)$lower, upper = P(sim)$upper, control = do.call("DEoptim.control", control),
-                 rasters = rasters, formula = P(sim)$formula, loci = loci, sizes = sizes, fireSense_SpreadFitRaster = fireSense_SpreadFitRaster) %>%
-    `[[` ("optim") %>% `[[` ("bestmem")
-
+  DE <- DEoptim(objfun, lower = P(sim)$lower, upper = P(sim)$upper, control = do.call("DEoptim.control", control),
+                 rasters = rasters, formula = P(sim)$formula, loci = loci, sizes = sizes, fireSense_SpreadFitRaster = fireSense_SpreadFitRaster)
+  
+  val <- DE %>% `[[` ("optim") %>% `[[` ("bestmem")
+  AD <- DE %>% `[[` ("optim") %>% `[[` ("bestval")
+  
   sim$fireSense_SpreadFitted <- 
     list(formula = P(sim)$formula,
-         coef = val %>% setNames(nm = c("A", "B", "D", "G", if (attr(terms, "intercept")) "Intercept" else NULL, attr(terms, "term.labels"))))
+         coef = val %>% setNames(nm = c("A", "B", "D", "G", if (attr(terms, "intercept")) "Intercept" else NULL, attr(terms, "term.labels"))),
+         AD = AD)
   class(sim$fireSense_SpreadFitted) <- "fireSense_SpreadFit"
   
   if (!is.na(P(sim)$intervalRunModule))
---
+++ tests/Test_SpreadFit.R
@@ -0,0 +1,48 @@
+library(SpaDES)
+
+# Define simulation parameters
+times <- list(start = 1, end = 1, timeunit = "year")
+modules <- list("fireSense_SpreadFit")
+paths <- list(
+  modulePath = file.path("~/Documents/GitHub/McIntire-lab/modulesPrivate/") ## TODO: change this to /path/to/modules/
+)
+
+# Define module parameters
+parameters <- list(
+  fireSense_SpreadFit = list(
+    formula = ~ beta + theta - 1,
+    lower = c(.2, .1, .01, .3, 0.001, 0.001),
+    upper = c(.5, 10, .2, 4, .3, .3),
+    trace = 5,
+    nCores = 1,
+    itermax = 5
+  )
+)
+
+# Define from where and how data will be loaded in the simList environment
+inputs <- data.frame(
+  objectName = c("fires", "beta", "theta"),
+  file = c("Z:/fires.shp", "Z:/beta__STACK.tif", "Z:/theta__STACK.tif"),
+  fun = c("shapefile", "stack", "stack"),
+  package = c("raster", "raster", "raster"),
+  loadTime = 1
+)
+
+# Create the simList
+sim <- simInit(
+  times = times,
+  modules = modules,
+  paths = paths,
+  params = parameters,
+  inputs = inputs
+)
+
+# All fires at the same time
+loadFiles(sim)
+sim[["fires"]][["date"]] <- NULL
+sim[["beta"]] <- setNames(raster::unstack(sim[["beta"]])[[1]], "beta")
+sim[["theta"]] <- setNames(raster::unstack(sim[["theta"]])[[1]], "theta")
+inputs(sim) <- list()
+
+sim <- spades(sim)
+sim$fireSense_SpreadFitted
