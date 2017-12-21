337c5337266e869d872c9b68ffc92d18fb352140
JM <jean.d.marchal@gmail.com>
2017-12-21 16:58:45 -0500

    Misc

--- fireSense_SpreadFit.R
+++ fireSense_SpreadFit.R
@@ -209,8 +209,7 @@ fireSense_SpreadFitRun <- function(sim)
     if (s <- sum(missing))
       stop(paste0(moduleName, "> '", allxy[missing][1L], "'",
                   if (s > 1) paste0(" (and ", s-1L, " other", if (s>2) "s", ")"),
-                  " not found in data objects nor in the simList environment."))
-    
+                  " not found in data objects."))
     
     rasters <- mget(allxy, envir = envData, inherits = FALSE) %>% stack
     
