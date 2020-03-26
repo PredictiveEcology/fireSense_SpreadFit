f82f063aef0a013a8f5a7f03a0808f3559c3c2ef
Tati Micheletti <tati.micheletti@gmail.com>
2020-03-25 18:36:39 -0700

    think its fxed. darn envs

--- R/doDataChecks.R
+++ R/doDataChecks.R
@@ -1,12 +1,12 @@
-.doDataChecks <- function(moduleName, env, attribs, fml){
+.doDataChecks <- function(moduleName, envir, attribs, fml){
   
-  if (is.null(env[["fireAttributesFireSense_SpreadFit"]]))
+  if (is.null(envir[["fireAttributesFireSense_SpreadFit"]]))
     stop(moduleName, "> '", attribs, "' not found in data objects or NULL.")
   
-  if (!is(env[["fireAttributesFireSense_SpreadFit"]], "SpatialPointsDataFrame"))
+  if (!is(envir[["fireAttributesFireSense_SpreadFit"]], "SpatialPointsDataFrame"))
     stop(moduleName, "> '", attribs, "' is not a SpatialPointsDataFrame.")
   
-  if (is.null(env[["fireAttributesFireSense_SpreadFit"]][["size"]]))
+  if (is.null(envir[["fireAttributesFireSense_SpreadFit"]][["size"]]))
     stop(moduleName, "> The SpatialPointsDataFrame '", attribs, "' must have a 'size' column.")
 
   if (is.empty.model(fml))
