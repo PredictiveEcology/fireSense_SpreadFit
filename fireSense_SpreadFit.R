a6d3689e3d1b737df08baa140949d5d9623e794a
Eliot McIntire <eliotmcintire@gmail.com>
2026-06-02 10:01:23 -0700

    use known Txt object names from fireSenseUtils

--- fireSense_SpreadFit.R
+++ fireSense_SpreadFit.R
@@ -27,7 +27,7 @@ defineModule(sim, list(
                   "PredictiveEcology/pemisc@development",
                   "PredictiveEcology/clusters@main (>=0.0.19)",
                   "PredictiveEcology/Require@development (>= 0.3.1)",
-                  "PredictiveEcology/fireSenseUtils@development (>= 0.0.6.9008)",
+                  "PredictiveEcology/fireSenseUtils@development (>= 0.2.0.9000)",
                   "PredictiveEcology/SpaDES.tools@development (>= 2.0.4.9002)"),
   parameters = rbind(
     defineParameter(".plots", "character|logical", default = NULL, ## TODO: use .plotInitialTime etc.
@@ -350,8 +350,8 @@ doEvent.fireSense_SpreadFit = function(sim, eventTime, eventType, debug = FALSE)
         numIterations <- length(sim$DE)
         
         # This is normally OK, but there are cached calls that are recovering the wrong ones.
-        if (!all(sim$spreadFitAdditionalColNames %in% fireSenseUtils::spreadFitAdditionalColNames)) {
-          sim$spreadFitAdditionalColNames <- fireSenseUtils::spreadFitAdditionalColNames
+        if (!all(sim$spreadFitAdditionalColNames %in% fireSenseUtils::spreadFitAdditionalColNamesTxt)) {
+          sim$spreadFitAdditionalColNames <- fireSenseUtils::spreadFitAdditionalColNamesTxt
         }
         
         df <- data.frame(I(list(numIterations)),
@@ -531,7 +531,7 @@ spreadFitPrep <- function(sim) {
 
   IDvar <- grep("ID", names(sim$spreadFirePoints[[1]]), value = TRUE) |> setdiff("GID")
   sim$lociList <- makeLociList(ras = sim$rasterToMatch, pts = sim$spreadFirePoints, idsCol = IDvar,
-                               yearPrefix = fireSenseUtils::yearChar)
+                               yearPrefix = fireSenseUtils::yearTxt)
 
   keepNames <- intersect(names(sim$fireSense_annualSpreadFitCovariates), names(sim$fireBufferedListDT))
   mod$covsX1000 <- covsX1000AndSetDF(
@@ -867,7 +867,7 @@ estimateSpreadParams <- function(fireSense_spreadFormula, anyAnnualCovariates, w
   # }
 
   if (!suppliedElsewhere("spreadFitAdditionalColNames")) {
-    sim$spreadFitAdditionalColNames <- fireSenseUtils::spreadFitAdditionalColNames
+    sim$spreadFitAdditionalColNames <- fireSenseUtils::spreadFitAdditionalColNamesTxt
   }
 
   return(invisible(sim))
@@ -882,4 +882,4 @@ plotParamsBest <- function(paramsBest) {
 }
 
 
-youngAge <- fireSenseUtils::youngAgeName
+youngAge <- fireSenseUtils::youngAgeTxt
